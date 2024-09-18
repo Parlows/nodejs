0bd4c9af00eb49b911e5286b58f80daa6e67dca0
Pablo Regodón Cerezo <65611953+Parlows@users.noreply.github.com>
2024-09-18 12:09:21 +0200

    Delete rebuild.sh

--- rebuild.sh
+++
@@ -1,4 +0,0 @@
-docker stop node_streaming
-docker rm node_streaming
-docker build -t node-app .
-docker run --name node_streaming -p 3000:3000 -v /media/my_ftp/Proyecto_vclip/raw_videos:/videos node-app
