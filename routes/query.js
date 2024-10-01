c9ae3e9e5f8995a822b788aece1d5058ec49cf22
Parlows <pablo.regodon@hotmail.com>
2024-10-01 10:27:41 +0200

    option to select encoder and post req to emb engine

--- routes/query.js
+++ routes/query.js
@@ -100,95 +100,131 @@ router.get('/qdrant', (req, res) => {
 
 router.get('/milvus', (req, res) => {
     const textQuery = req.query.text;
+    const encoderQuery = req.query.encoder;
+
+    let encoderName = ''
+    let collectionName = ''
+    switch (encoderQuery) {
+        case 'CLIP':
+            encoderName = 'clip';
+            collectionName = 'ucfclipcentroid'
+            break;
+        case 'VCLIP':
+            encoderName = 'vclip';
+            collectionName = 'ucfvclipcentroid'
+            break;
+    }
     
     if(!textQuery) {
         return res.status(400);
-    }
+    };
 
     // Get embedding
     embUrl = `http://${process.env.EMB_ENGINE_HOST}:${process.env.EMB_ENGINE_PORT}` +
-        `/text?text=${textQuery}`
+        `/text?text=${textQuery}`;
 
-    http.get(embUrl, embRes => {
-            let emb = ''
-            
-            embRes.on('data', chunk => {
-                emb += chunk;
-            });
+    const embData = JSON.stringify({
+        encoder: encoderName,
+        data: textQuery
+    });
 
-            embRes.on('end', () => {
-                try {
-                    console.log("Sending embedding")
-
-                    // Data to be sent in the POST request
-                    const postData = JSON.stringify({
-                        collectionName: 'ucfclipcentroid',
-                        outputFields: ['video', 'start_frame', 'end_frame', 'sentence'],
-                        limit: 10,
-                        offset: 0,
-                        data: JSON.parse(emb)
-                    });
-                
-                    // Options for the POST request
-                    const options = {
-                        hostname: process.env.MILVUS_HOST,
-                        port: process.env.MILVUS_PORT,
-                        path: '/v2/vectordb/entities/search',
-                        method: 'POST',
-                        headers: {
-                        'Content-Type': 'application/json',
-                        'Content-Length': Buffer.byteLength(postData),
-                        'Host': `${process.env.MILVUS_HOST}:${process.env.MILVUS_PORT}`
-                        }
-                    };
-                
-                    const qdrantReq = http.request(options, qdrantRes => {
-                        let qdrantData = '';
-                        
-                        // Collect data chunks as they arrive
-                        qdrantRes.on('data', (chunk) => {
-                            qdrantData += chunk;
-                        });
-                    
-                        // Process the complete response once all chunks are received
-                        qdrantRes.on('end', () => {
-                        try {
-                            const results = JSON.parse(qdrantData);
-
-                            resultsArray = []
-                            
-                            results.data.forEach(element => {
-                                resultsArray.push({
-                                    video: element.video,
-                                    start_frame: element.start_frame,
-                                    end_frame: element.end_frame,
-                                    sentence: element.sentence
-                                })
-                            });
-
-                            const clientRes = {results: resultsArray}
-
-                            res.json(clientRes);
-                        } catch (error) {
-                            res.status(500)
-                        }
-                        });
-                    });
-
-                    // Handle errors in the QDrant request
-                    qdrantReq.on('error', (e) => {
-                        res.status(500).json({ error: `QDrant request error: ${e.message}` });
-                    });
-            
-                    // Write the data to the QDrant request body
-                    qdrantReq.write(postData);
-                    qdrantReq.end();
-                } catch (err) {
-                    res.status(500);
-                }
-            });
+    const embOptions = {
+        hostname: process.env.EMB_ENGINE_HOST,
+        port: process.env.EMB_ENGINE_PORT,
+        path: '/text',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': embData.length
         }
-    );
+    };
+
+    console.log('Sending req to embedding engine');
+    const embRequest = http.request(embOptions, embRes => {
+        let embJSON = '';
+        
+        embRes.on('data', chunk => {
+            embJSON += chunk.toString(); // Store response as string as it comes by
+        });
+
+        embRes.on('end', () => {
+            try {
+                console.log("Sending embedding")
+                let emb = embJSON
+                console.log(emb)
+
+                // Data to be sent in the POST request
+                const postData = JSON.stringify({
+                    collectionName: collectionName,
+                    outputFields: ['video', 'start_frame', 'end_frame', 'sentence'],
+                    limit: 10,
+                    offset: 0,
+                    data: JSON.parse(emb)
+                });
+            
+                // Options for the POST request
+                const options = {
+                    hostname: process.env.MILVUS_HOST,
+                    port: process.env.MILVUS_PORT,
+                    path: '/v2/vectordb/entities/search',
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': Buffer.byteLength(postData),
+                    'Host': `${process.env.MILVUS_HOST}:${process.env.MILVUS_PORT}`
+                    }
+                };
+            
+                const qdrantReq = http.request(options, qdrantRes => {
+                    let qdrantData = '';
+                    
+                    // Collect data chunks as they arrive
+                    qdrantRes.on('data', (chunk) => {
+                        qdrantData += chunk;
+                    });
+                
+                    // Process the complete response once all chunks are received
+                    qdrantRes.on('end', () => {
+                    try {
+                        console.log('Processing Milvus Response..')
+                        const results = JSON.parse(qdrantData);
+                        console.log(results);
+                        resultsArray = []
+                        
+                        results.data.forEach(element => {
+                            resultsArray.push({
+                                video: element.video,
+                                start_frame: element.start_frame,
+                                end_frame: element.end_frame,
+                                sentence: element.sentence
+                            })
+                        });
+
+                        const clientRes = {results: resultsArray}
+
+                        res.json(clientRes);
+                    } catch (error) {
+                        res.status(500)
+                    }
+                    });
+                });
+
+                // Handle errors in the QDrant request
+                qdrantReq.on('error', (e) => {
+                    res.status(500).json({ error: `QDrant request error: ${e.message}` });
+                });
+        
+                // Write the data to the QDrant request body
+                qdrantReq.write(postData);
+                qdrantReq.end();
+            } catch (err) {
+                res.status(500);
+            }
+        });
+    });
+
+    embRequest.write(embData);
+    embRequest.end();
 });
 
 module.exports = router;
