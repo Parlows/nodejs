6ce42fda19950dba4fad7b0b7fc1d08cddb3ba9b
Parlows <pablo.regodon@hotmail.com>
2024-10-02 09:02:17 +0200

    added comments

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+
+// Add express
 const express = require('express');
+
+// Add .env info
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Instantiate application
 const app = express();
 
+// Add static content
 app.use(express.static('public'));
 
-const queryRouter = require('./routes/query')
+// Add routes to query database
+const queryRouter = require('./routes/query.js')
 app.use('/query', queryRouter);
+
+// Add routes to send videos
 const streaming = require('./routes/streaming')
 app.use('/video', streaming)
 
-app.listen(3000, () => {
+// Set up the server
+app.listen(process.env.NODE_PORT, () => {
     console.log('Server running in port 3000')
 });
 
